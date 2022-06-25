aa7e3154765fc5eaf6578bd484f9f2bce17cc2e4
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-06-26 00:47:07 +0800

    Restructure folders, and serve HTML files as response

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 //run this commands to use this project
 //npm install express
 //npm install --save-dev nodemon 
+const path = require('path');
 
 const express = require('express');
 
 const app = express();
 
 app.get('/', function(req, res){
-    res.send("Hello world!");
+    const htmlfilePath = path.join(__dirname, 'views', 'index.html');
+    res.sendFile(htmlfilePath);
+});
+
+app.get('/about', function(req, res){
+    const htmlfilePath = path.join(__dirname, 'views', 'about.html');
+    res.sendFile(htmlfilePath);
+});
+app.get('/confirm', function(req, res){
+    const htmlfilePath = path.join(__dirname, 'views', 'confirm.html');
+    res.sendFile(htmlfilePath);
+});
+app.get('/recommend', function(req, res){
+    const htmlfilePath = path.join(__dirname, 'views', 'recommend.html');
+    res.sendFile(htmlfilePath);
+});
+app.get('/restaurants', function(req, res){
+    const htmlfilePath = path.join(__dirname, 'views', 'restaurants.html');
+    res.sendFile(htmlfilePath);
 });
 
 app.listen(3000);
