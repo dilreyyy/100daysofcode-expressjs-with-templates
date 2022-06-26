3a9b0ac9422563e4a3dd45f84941bcec3504767a
Its Ya Boi Del Rey <delreeyyy@gmail.com>
2022-06-26 11:44:49 +0800

    Parsing form data, redirecting requests, store in json file

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 //npm install express
 //npm install --save-dev nodemon 
 const path = require('path');
-
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+
+app.use(express.urlencoded({extended: false}));
 
 app.use(express.static('public'));
 
@@ -25,6 +27,18 @@ app.get('/confirm', function(req, res){
 app.get('/recommend', function(req, res){
     const htmlfilePath = path.join(__dirname, 'views', 'recommend.html');
     res.sendFile(htmlfilePath);
+});
+
+app.post('/recommend', function(req, res){
+    const restaurants = req.body;
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+    const fileData = fs.readFileSync(filePath);
+
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurants);
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+    res.redirect('/confirm');
 });
 app.get('/restaurants', function(req, res){
     const htmlfilePath = path.join(__dirname, 'views', 'restaurants.html');
